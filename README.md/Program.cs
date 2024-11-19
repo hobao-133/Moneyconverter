7c48a439e75596814fe24ccd48076dc7a9aecbc4
hobao-133 <kevinbh133@gmail.com>
2024-11-19 21:25:40 +0700

    first commit

---
+++ README.md/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+class Program{
+    static void Main(string[]args){
+        const double USD_TO_VND = 23000;
+        Console.WriteLine("Chào mừng đến ứng dụng chuyển đổi tiền tệ!");
+        Console.WriteLine("Chuyển đổi từ USD sang VND");
+        Console.WriteLine("Nhập số tiền cần đổi");
+        double amount = double.Parse(Console.ReadLine());
+        double result = 0;
+        result = amount * USD_TO_VND;
+        Console.WriteLine($"{amount} USD = {result} VND");
+    }
+}
